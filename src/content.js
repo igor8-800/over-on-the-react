3499de9bc5b227573851a97513fec23af951ae31
igor <87110200+igor8-800@users.noreply.github.com>
2021-11-04 11:16:31 +0700

    v 1.1

--- src/content.js
+++ src/content.js
@@ -49,6 +49,125 @@ class NewsWindow extends React.Component {
 	}
 }
 
+
+
+
+
+class ContentTitle extends React.Component {
+	render() {
+
+		return (
+			<div className="content__title">
+				<p>Introducing Horror Fellow</p>
+			</div>
+		);
+
+	}
+};
+class ContentText extends React.Component {
+	render() {
+
+		return (
+			<div className="content__text">
+				<p>SOLD OUT - 10,000 uniquely generated, cute and collectible pixel art souls with proof of ownership stored on the Solana blockchain.</p>
+			</div>
+		);
+
+	}
+};
+
+
+
+class ContentButton extends React.Component {
+	render() {
+		return (
+
+			<form action="" className="content__button">
+				<button>Buy on DigitalEyes Marketplace</button>
+			</form>
+
+
+		)
+
+	}
+}
+
+
+class ContentTextTwe extends React.Component {
+	render() {
+
+		return (
+			<div className="content__text__twe">
+				<p>Our mint is over,  our team is busy working on our roadmap.
+					However, if you're looking to launch your own NFTs  join the Solana community - we can help</p>
+			</div>
+		);
+
+	}
+};
+
+class ContentIMG extends React.Component {
+	render() {
+
+		return (
+			<div className="content__IMG">
+			</div>
+		);
+
+	}
+};
+class ContentBlock extends React.Component {
+	render() {
+
+
+		return (
+
+			<div >
+				<ContentTitle />
+				<ContentText />
+				<ContentButton />
+				<ContentTextTwe />
+			</div>
+
+
+		)
+
+	}
+};
+class ContentInfos extends React.Component {
+	render() {
+
+
+		return (
+
+			<div className="content__infos">
+				<ContentBlock />
+				<ContentIMG />
+			</div>
+
+
+		)
+
+	}
+};
+
+class ContenFull extends React.Component {
+	render() {
+
+
+
+
+
+		return (
+			<div className="content__block">
+				<NewsWindow />
+				<ContentInfos />
+
+			</div>
+		)
+	}
+}
+
 class Content extends React.Component {
 	render() {
 
@@ -57,7 +176,7 @@ class Content extends React.Component {
 
 
 		return (
-			<NewsWindow />
+			<ContenFull />
 		)
 	}
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Header from './App2';
+import Content from './content';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<main>
+		<Header />
+		<Content />
+	</main>,
+	document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
